d148947f18abd51e9c4c9fd30ed76646e2676e56
wangyunquan <wangyunquan@live.com>
2017-06-28 17:10:07 +0800

    add adminlte

--- src/main/java/com/buswe/Constants.java
+++ src/main/java/com/buswe/Constants.java
@@ -8,7 +8,7 @@ public final class Constants {
     public static final String ENV_PRODUCTION = "production";
 
     public static final String ENV_DEVELOPMENT = "development";
-
+   
     public static final String DEFAULT_ADMIN_EMAIL = "admin@admin.com";
 
     public static final String DEFAULT_ADMIN_PASSWORD = "admin";
